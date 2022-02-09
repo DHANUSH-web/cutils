1b281ef7b3e864e47e619918809d23258de41cfd
DHANUSH H V <67680433+DHANUSH-web@users.noreply.github.com>
2022-02-09 14:23:27 +0530

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,45 @@
+// Stack Usage in CUTILS
+
+#include "src/cutils.hpp"
+
+int main()
+{
+	// initial array
+	vector<int> arr({1, 2, 5, 20});
+
+	// stack
+	Stack<int> stk = Stack<int>(arr);
+
+	// print the initial Stack
+	cout << "Initial Stack" << endl;
+	stk.printStack();	// 1 2 5 20
+
+	// pop method
+	cout << "\nPop Method" << endl;
+	stk.pop();
+	stk.printStack();	// 1 2 5
+
+	// Push Method
+	cout << "\nPush Method" << endl;
+	stk.push(23);
+	stk.printStack();	// 1 2 5 23
+
+	// Insert method
+	cout << "\nInsert Method" << endl;
+	stk.insert(1, 21);
+	stk.printStack();	// 1 21 2 5 23
+
+	// Remove method
+	cout << "\nRemove Method" << endl;
+	stk.remove(2);
+	stk.printStack();	// 1 21 5 23
+
+	// print the array passed
+	cout << "\nPassed Array: ";
+
+	for (int i = 0; i < stk.arr.size(); i++)
+		cout << stk.arr[i] << " ";
+
+	cout << endl;
+	return 0;
+}
